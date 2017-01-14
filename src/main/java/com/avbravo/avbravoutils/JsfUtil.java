27fd9a4d3086572a43d298af8c853dddac38bed3
avbravo <avbravo@avbravo>
2017-01-14 13:18:11 -0500

    0.1.4.1 letter

--- src/main/java/com/avbravo/avbravoutils/JsfUtil.java
+++ src/main/java/com/avbravo/avbravoutils/JsfUtil.java
@@ -489,6 +489,46 @@ public class JsfUtil implements Serializable {
     }
 
     
-   
+   public  String letterToUpper(String texto) {
+        try {
+
+            texto = texto.trim();
+            int largo = texto.length();
+            if (largo <= 0) {
+                return texto;
+            }
+            String letra = texto.substring(0, 1);
+
+            texto = letra.toUpperCase() + texto.substring(1);
+        } catch (Exception ex) {
+          System.out.println("letterToUpper() " + ex.getLocalizedMessage());
+        }
+        return texto;
+    }
+
+    /**
+     * ConvertirLetraMinuscula
+     *
+     * @param s_cadena
+     * @param caracter
+     * @return
+     */
+    public  String letterToLower(String texto) {
+
+        try {
+
+            texto = texto.trim();
+            int largo = texto.length();
+            if (largo <= 0) {
+                return texto;
+            }
+            String letra = texto.substring(0, 1);
+
+            texto = letra.toLowerCase() + texto.substring(1);
+        } catch (Exception ex) {
+          System.out.println("letterToLower() " + ex.getLocalizedMessage());
+        }
+        return texto;
+    }
     
 }
