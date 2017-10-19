069007a07aae013e519d1377b4d74c3c1b9f233e
avbravo <avbravo@avbravo>
2017-10-18 20:06:11 -0500

    0.11.12

---
+++ src/main/java/com/avbravo/avbravoutils/pojos/UserLogUtils.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.avbravoutils.pojos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author avbravo
+ */
+public class UserLogUtils {
+    private String username;
+    private Date datetime;
+    private String description;
+
+    public UserLogUtils() {
+    }
+
+    public UserLogUtils(String username, Date datetime, String description) {
+        this.username = username;
+        this.datetime = datetime;
+        this.description = description;
+    }
+
+    
+    
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+}
