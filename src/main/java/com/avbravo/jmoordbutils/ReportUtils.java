62cf925f6ac3cbc87daa8aca1d562584c7b5684a
avbravo <avbravo@avbravo>
2019-09-09 14:05:12 -0500

    0.28

---
+++ src/main/java/com/avbravo/jmoordbutils/ReportUtils.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.jmoordbutils;
+
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPCell;
+import java.io.Serializable;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author avbravo
+ */
+public class ReportUtils implements Serializable {
+
+// <editor-fold defaultstate="collapsed" desc="Paragraph paragraph(String texto, Font fonFactory,Integer ALIGN)">
+    /**
+     * Paragraph p = new Paragraph("MI TEXTO", FontFactory.getFont("arial", //
+     * fuente 12, // tamaño Font.BOLD)); p.setAlignment(Element.ALIGN_CENTER);
+     *
+     * @param texto "Mi Texto"
+     * @param fonFactory FontFactory.getFont("arial",12, Font.BOLD);
+     * @param ALIGN: Element.ALIGN_CENTER
+     * @return
+     */
+    public static Paragraph paragraph(String texto, Font fonFactory, Integer ALIGN) {
+        Paragraph p = new Paragraph(texto,
+                FontFactory.getFont("arial", // fuente
+                        12, // tamaño
+                        Font.BOLD));
+        p.setAlignment(Element.ALIGN_CENTER);
+        return p;
+    }
+
+    // </editor-fold>  
+    // <editor-fold defaultstate="collapsed" desc="metodo()">
+    public static PdfPCell PdfCell(String texto, Font fontFactory, Integer ALIGN) {
+
+        PdfPCell cell = new PdfPCell(new Paragraph(texto, fontFactory));
+        cell.setHorizontalAlignment(ALIGN);
+        return cell;
+    }
+    // </editor-fold>  
+
+    // <editor-fold defaultstate="collapsed" desc="printPDF(ByteArrayOutputStream baos)">
+    public static Boolean printPDF(ByteArrayOutputStream baos) {
+        try {
+            FacesContext context = FacesContext.getCurrentInstance();
+            Object response = context.getExternalContext().getResponse();
+            if (response instanceof HttpServletResponse) {
+                HttpServletResponse hsr = (HttpServletResponse) response;
+                hsr.setContentType("application/pdf");
+                hsr.setHeader("Contentdisposition", "attachment;filename=report.pdf");
+                //        hsr.setHeader("Content-disposition", "attachment");
+                hsr.setContentLength(baos.size());
+                try {
+                    ServletOutputStream out = hsr.getOutputStream();
+                    baos.writeTo(out);
+                    out.flush();
+                } catch (IOException ex) {
+                    System.out.println("Error:  " + ex.getMessage());
+                    return false;
+                }
+                context.responseComplete();
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+    // </editor-fold> 
+    
+    // <editor-fold defaultstate="collapsed" desc=" ByteArrayOutputStream initializate()">
+    public static  ByteArrayOutputStream initializate(){
+         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+         return baos;
+    }
+    // </editor-fold>  
+    
+  
+            
+}
