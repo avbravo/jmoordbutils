965a9a6f044b9d80105fc54bc3480ead819a660d
avbravo <avbravo@avbravo>
2020-03-15 12:05:29 -0500

    0.63, FileDecryption, FileEncryption

---
+++ src/main/java/com/avbravo/jmoordbutils/fileencripter/FileDecryption.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.jmoordbutils.fileencripter;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.spec.KeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author avbravo
+ */
+public class FileDecryption {
+
+    public static Boolean desencriptarFile(String fileEnc, String fileIvEnc, String password, String extension) {
+
+        try {
+            String decryptedFile= fileEnc.substring(0, fileEnc.lastIndexOf('.'))+"_decrypted."+extension;
+            // reading the salt
+            // user should have secure mechanism to transfer the
+            // salt, iv and password to the recipient
+            FileInputStream saltFis = new FileInputStream(fileEnc);
+            byte[] salt = new byte[8];
+            saltFis.read(salt);
+            saltFis.close();
+
+            // reading the iv
+            FileInputStream ivFis = new FileInputStream(fileIvEnc);
+            byte[] iv = new byte[16];
+            ivFis.read(iv);
+            ivFis.close();
+
+            SecretKeyFactory factory = SecretKeyFactory
+                    .getInstance("PBKDF2WithHmacSHA1");
+            KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
+                    256);
+            SecretKey tmp = factory.generateSecret(keySpec);
+            SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            // file decryption
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+            FileInputStream fis = new FileInputStream("encryptedfile.des");
+            FileOutputStream fos = new FileOutputStream(decryptedFile);
+            byte[] in = new byte[64];
+            int read;
+            while ((read = fis.read(in)) != -1) {
+                byte[] output = cipher.update(in, 0, read);
+                if (output != null) {
+                    fos.write(output);
+                }
+            }
+
+            byte[] output = cipher.doFinal();
+            if (output != null) {
+                fos.write(output);
+            }
+            fis.close();
+            fos.flush();
+            fos.close();
+            System.out.println("File Decrypted.");
+            return true;
+        } catch (Exception ex) {
+            System.out.println("desencriptarFile() " + ex.getLocalizedMessage());
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/avbravo/jmoordbutils/fileencripter/FileEncryption.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.jmoordbutils.fileencripter;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.AlgorithmParameters;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author avbravo Fuente:
+ * https://javapapers.com/java/java-file-encryption-decryption-using-aes-password-based-encryption-pbe/
+ */
+public class FileEncryption {
+
+    public static Boolean cifrar(String filenamepath, String password) {
+        try {
+           String filenameEnc = filenamepath.substring(0, filenamepath.lastIndexOf('.'))+".enc";
+           String filenameIvEnc = filenamepath.substring(0, filenamepath.lastIndexOf('.'))+"iv.enc";
+
+            // password to encrypt the file
+            //String password = "javapapers";
+            // file to be encrypted
+            FileInputStream inFile = new FileInputStream(filenamepath);
+
+            // encrypted file
+            FileOutputStream outFile = new FileOutputStream("encryptedfile.des");
+
+            // password, iv and salt should be transferred to the other end
+            // in a secure manner
+            // salt is used for encoding
+            // writing it to a file
+            // salt should be transferred to the recipient securely
+            // for decryption
+            byte[] salt = new byte[8];
+            SecureRandom secureRandom = new SecureRandom();
+            secureRandom.nextBytes(salt);
+            FileOutputStream saltOutFile = new FileOutputStream(filenameEnc );
+            saltOutFile.write(salt);
+            saltOutFile.close();
+
+            SecretKeyFactory factory = SecretKeyFactory
+                    .getInstance("PBKDF2WithHmacSHA1");
+            KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
+                    256);
+            SecretKey secretKey = factory.generateSecret(keySpec);
+            SecretKey secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+
+            //
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secret);
+            AlgorithmParameters params = cipher.getParameters();
+
+            // iv adds randomness to the text and just makes the mechanism more
+            // secure
+            // used while initializing the cipher
+            // file to store the iv
+            FileOutputStream ivOutFile = new FileOutputStream(filenameIvEnc);
+            byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+            ivOutFile.write(iv);
+            ivOutFile.close();
+
+            //file encryption
+            byte[] input = new byte[64];
+            int bytesRead;
+
+            while ((bytesRead = inFile.read(input)) != -1) {
+                byte[] output = cipher.update(input, 0, bytesRead);
+                if (output != null) {
+                    outFile.write(output);
+                }
+            }
+
+            byte[] output = cipher.doFinal();
+            if (output != null) {
+                outFile.write(output);
+            }
+
+            inFile.close();
+            outFile.flush();
+            outFile.close();
+
+            System.out.println("File Encrypted.");
+            return true;
+        } catch (Exception ex) {
+            System.out.println("cifrar() " + ex.getLocalizedMessage());
+        }
+        return false;
+    }
+
+}
