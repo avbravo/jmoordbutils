65345912d5d9bb194d3648fbb87d97dc3a8d4645
avbravo <avbravo@avbravo>
2020-08-10 12:01:06 -0500

    0.88.2

--- src/main/java/com/avbravo/jmoordbutils/JmoordbLanguages.java
+++ src/main/java/com/avbravo/jmoordbutils/JmoordbLanguages.java
@@ -7,6 +7,7 @@ package com.avbravo.jmoordbutils;
 
 import java.io.Serializable;
 import java.util.Locale;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -24,14 +25,23 @@ public class JmoordbLanguages implements Serializable {
     @Inject
     JmoordbResourcesFiles rf;
     private String locale = Locale.getDefault().getDisplayLanguage();
+    Locale infoLocaleDate = Locale.getDefault();
+
+    private String bandera;
+
+    @PostConstruct
+    public void init() {
+        bandera = "Paname32.png";
+        spanishAction();
+    }
 
     public void setLocale(String locale) {
         this.locale = locale;
     }
 
     public synchronized String getLocale() {
-        if(locale== null){
-            locale="es";
+        if (locale == null) {
+            locale = "es";
         }
         return locale;
     }
@@ -41,12 +51,14 @@ public class JmoordbLanguages implements Serializable {
     }
 
     public JmoordbLanguages() {
-        this.locale="es";
+        this.locale = "es";
     }
 
     public String englishAction() {
+        bandera = "United-States.png";
         FacesContext context = FacesContext.getCurrentInstance();
         context.getViewRoot().setLocale(Locale.ENGLISH);
+        infoLocaleDate = Locale.ENGLISH;
         this.locale = "en";
         rf.saveLocale();
         return null;
@@ -55,9 +67,77 @@ public class JmoordbLanguages implements Serializable {
     public String spanishAction() {
         FacesContext context = FacesContext.getCurrentInstance();
         context.getViewRoot().setLocale(new Locale("es"));
+        infoLocaleDate = new Locale("es");
         this.locale = "es";
         rf.saveLocale();
         return null;
+    }
+
+    public String portugueseAction() {
+        bandera = "Brasil.png";
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale(new Locale("pt", "BR"));
+        infoLocaleDate = new Locale("pt", "BR");
+        this.locale = "es";
+        rf.saveLocale();
+        return null;
+
+    }
+
+    public String spanishSpainAction() {
+        bandera = "Espanha.png";
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale(new Locale("es", "ES"));
+        infoLocaleDate = new Locale("es", "ES");
+        this.locale = "es";
+        rf.saveLocale();
+        return null;
+    }
+
+    public String francesAction() {
+        bandera = "France.png";
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale(Locale.FRANCE);
+        infoLocaleDate = Locale.FRANCE;
+        this.locale = "fr";
+        rf.saveLocale();
+        return null;
+    }
+
+    public String alemaniaAction() {
+        bandera = "Germany.png";
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale(Locale.FRANCE);
+        infoLocaleDate = Locale.GERMAN;
+        this.locale = "gr";
+        rf.saveLocale();
+        return null;
+    }
+
+    public String italiaAction() {
+        bandera = "Brasil.png";
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale(Locale.ITALIAN);
+        infoLocaleDate = Locale.ITALIAN;
+        this.locale = "il";
+        rf.saveLocale();
+        return null;
+    }
+
+    public Locale infoLocale() {
+        try {
+
+            // Locale.getDefault().getLanguage();//       ---> en      
+//Locale.getDefault().getISO3Language()   ---> eng 
+//Locale.getDefault().getCountry()        ---> US 
+//Locale.getDefault().getISO3Country()    ---> USA 
+//Locale.getDefault().getDisplayCountry() ---> United States 
+//Locale.getDefault().getDisplayName()    ---> English (United States) 
+//Locale.getDefault().toString()          ---> en_US
+//Locale.getDefault().getDisplayLanguage()---> English
+        } catch (Exception e) {
+        }
+        return infoLocaleDate;
     }
 
 }
