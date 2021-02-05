67495e05e90ecaf78eb313dba708d0f15c344e12
avbravo <avbravo@avbravo>
2021-02-05 11:29:10 -0500

    1.27 Implmenta JmoordbMediaManager, JMoordbMediaContext

---
+++ src/main/java/com/avbravo/jmoordbutils/media/JMoordbMediaManager.java
@@ -0,0 +1,271 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.jmoordbutils.media;
+
+
+
+import com.avbravo.jmoordbutils.JsfUtil;
+import static com.avbravo.jmoordbutils.JsfUtil.errorDialog;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+/**
+ *
+ * @author avbravo
+ */
+@Named(value ="jMoordbMediaManager" )
+@RequestScoped
+public class JMoordbMediaManager implements Serializable {
+// <editor-fold defaultstate="collapsed" desc="field">
+    private StreamedContent media;
+
+    InputStream is = null;
+   // </editor-fold>
+    
+ 
+    // <editor-fold defaultstate="collapsed" desc="set/get">
+    public InputStream getIs() {
+        return is;
+    }
+
+    public void setIs(InputStream is) {
+        this.is = is;
+    }
+
+    public StreamedContent getMedia() {
+        return media;
+    }
+
+    public void setMedia(StreamedContent media) {
+        this.media = media;
+    }
+
+     // </editor-fold>
+    
+    
+    // <editor-fold defaultstate="collapsed" desc="init()">
+    
+    @PostConstruct
+    public void init() {
+        try {
+     //       System.out.println("init() GraphImageView..");
+ String fileDownloadPath = (String) JmoordbMediaContext.get("fileDownloadPath");
+
+            if (fileDownloadPath == null || fileDownloadPath.equals("")) {
+//                System.out.println("Es null en el context");
+            } else {
+               generate(fileDownloadPath);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+   // </editor-fold>
+    
+        // <editor-fold defaultstate="collapsed" desc="StreamedContent generate(String pathFile)">
+    public StreamedContent generate(String pathFile) {
+        try {
+//            System.out.println("-------------------------------------");
+//            System.out.println("------generateImage "+ pathFile);
+//            System.out.println("-------------------------------------");
+            String name = nameOfFileInPath(pathFile);
+            String pathOfFile =pathOfFile(pathFile);
+            String extensionOfFileInPath = extensionOfFileInPath(pathFile);
+
+            File filet = new File(pathFile);
+            is = new FileInputStream(filet);
+
+           media = DefaultStreamedContent.builder()
+                    .contentType(typeOfMimeTypeForDownload(extensionOfFileInPath))
+                    .name(name + extensionOfFileInPath)
+                    .stream(() -> is)
+                    .build();
+        } catch (Exception e) {
+            System.out.println("generateImage() " + e.getLocalizedMessage());
+            JsfUtil.errorMessage("generateImage()" + e.getLocalizedMessage());
+        }
+
+        return media;
+    }
+      // </editor-fold>
+    
+    
+       // <editor-fold defaultstate="collapsed" desc="String typeOfMimeTypeForDownload(String extension)">
+   
+    public static String typeOfMimeTypeForDownload(String extension){
+        String text ="";
+        try {
+    
+ 
+  switch(extension){
+            case "jpg":
+          text="image/jpg";
+                break;
+            case "png":
+               text="image/png";
+                break;
+            case "pdf":
+               text="application/pdf";
+                break;
+            case "plain":
+                 text ="text/plain";
+            case "cvs":
+                 text ="text/csv";
+           
+                break;
+                
+            case "txt":
+                 text="application/txt";
+           
+                break;
+                
+            default:
+               System.out.println("Esta extesnsion"+extension + " no es soportada...");
+        }
+        } catch (Exception e) {
+            System.out.println("typeOfMimeTypeForDownload() "+e.getLocalizedMessage());
+        }
+        return text;
+    }
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="String nameOfFileInPath(String filenamePath)">
+    /**
+     *
+     * @param filenamePath
+     * @return el nombre del archivo que esta en un path
+     */
+    public String nameOfFileInPath(String filenamePath) {
+        String name = "";
+        try {
+            name = filenamePath.substring(filenamePath.lastIndexOf(System.getProperty("file.separator")) + 1,
+                    filenamePath.lastIndexOf('.'));
+        } catch (Exception e) {
+            errorDialog("nameOfFileInPath()", e.getLocalizedMessage());
+        }
+        return name;
+        
+    }
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="String pathOfFile(String filenamePath)" >
+    /**
+     *
+     * @param filenamePath
+     * @return el path de un archivo
+     */
+    public String pathOfFile(String filenamePath) {
+        String path = "";
+        try {
+            path = filenamePath.substring(0, filenamePath.lastIndexOf(System.getProperty("file.separator")));
+        } catch (Exception e) {
+            errorDialog("pathOfFile()", e.getLocalizedMessage());
+        }
+        return path;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="String extensionOfFileInPath(String filenamePath)">
+    /**
+     *
+     * @param filenamePath
+     * @return devuelve la extension de un archivo en un path
+     */
+    public  String extensionOfFileInPath(String filenamePath) {
+        String extension = "";
+        try {
+            extension = filenamePath.substring(filenamePath.lastIndexOf('.') + 1, filenamePath.length());
+        } catch (Exception e) {
+             errorDialog("extensionOfFileInPathe()", e.getLocalizedMessage());
+        }
+        return extension;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc=" Boolean isImage(String... filenamePath))">
+    /**
+     *
+     * @param Verifica si es una imagen
+     * @return devuelve la extension de un archivo en un path
+     */
+    public Boolean isImage(String... filenamePath) {
+        String path = "";
+        String extension=""; 
+        Boolean valid = false;
+           
+        try {
+             if (filenamePath.length != 0) {
+                path = filenamePath[0];
+
+            }else{
+                 path = (String) JmoordbMediaContext.get("fileDownloadPath");
+
+            if (path== null || path.equals("")) {
+                path="";
+             }
+             }
+             if(!path.equals("")){
+                 extension = path.substring(path.lastIndexOf('.') + 1, path.length());
+           
+                 if(extension.equals("jpg") || extension.equals("png")){
+                     valid=true;
+                 }
+             }
+            
+            
+        } catch (Exception e) {
+             errorDialog("isImage()", e.getLocalizedMessage());
+        }
+        return valid;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc=" Boolean isPDF(String... filenamePath)">
+    /**
+     *
+     * @param Verifica si es una imagen
+     * @return devuelve la extension de un archivo en un path
+     */
+    public Boolean isPDF(String... filenamePath) {
+        String path = "";
+        String extension=""; 
+        Boolean valid = false;
+           
+        try {
+             if (filenamePath.length != 0) {
+                path = filenamePath[0];
+
+            }else{
+                 path = (String) JmoordbMediaContext.get("fileDownloadPath");
+
+            if (path== null || path.equals("")) {
+                path="";
+             }
+             }
+             if(!path.equals("")){
+                 extension = path.substring(path.lastIndexOf('.') + 1, path.length());
+
+                 if(extension.equals("pdf")){
+                     valid=true;
+                 }
+             }
+            
+            
+        } catch (Exception e) {
+             errorDialog("isPDF()", e.getLocalizedMessage());
+        }
+        return valid;
+    }
+    // </editor-fold>
+    
+    
+}
---
+++ src/main/java/com/avbravo/jmoordbutils/media/JmoordbMediaContext.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.jmoordbutils.media;
+
+import static com.avbravo.jmoordbutils.JsfUtil.errorMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author avbravo
+ */
+public class JmoordbMediaContext {
+
+    // <editor-fold defaultstate="collapsed" desc="void put(String key, Object value)">
+    /**
+     * guarda en el session
+     * @param key
+     * @param value 
+     */
+    public static void put(String key, Object value) {
+        try{
+             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(key, value);
+        }catch(Exception e){
+              errorMessage("put()" + " " + e.getLocalizedMessage());
+        }  
+      
+       
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="putApplication()">
+    public static void putApplication(String key, Object value) {
+        try{
+             FacesContext.getCurrentInstance().getExternalContext().getApplicationMap().put(key, value);
+        }catch(Exception e){
+             errorMessage("put()" + " " + e.getLocalizedMessage());
+        }  
+      
+       
+    }
+    
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Object get(String key)">
+    public static Object get(String key) {
+        try{
+             return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(key);
+        }catch(Exception e){
+          errorMessage("get()" + " " + e.getLocalizedMessage());
+        }  
+      
+       return "";
+    }
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Object getApplication(String key)">
+    /**
+     * GetApplication obtiene el valor del application
+     * @param key
+     * @return 
+     */
+    public static Object getApplication(String key) {
+        try{
+             return FacesContext.getCurrentInstance().getExternalContext().getApplicationMap().get(key);
+        }catch(Exception e){
+             errorMessage("getApplication()" + " " + e.getLocalizedMessage());
+        }  
+      
+       return "";
+    }
+    // </editor-fold>
+}
